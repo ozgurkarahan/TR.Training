575bb07d185f3da3983d94b1d3bb0ba7d8be21b0
ozgur karahan <ozgur.karahan@keyconsulting.fr>
2013-02-18 01:41:11 +0100

    Exit when clients receives all callback messages

--- TR.Client/Program.cs
+++ TR.Client/Program.cs
@@ -17,43 +17,37 @@ namespace TR.Client
             IClient<ICalculateService, ICalculateCallBack> client = new NamedPipeClient();
             ICalculateService calculateService = client.Proxy.CreateChannel();
 
-            while (true)
-            {
-                Console.WriteLine("Enter a number to calculate or Type 'exit' for exit application");
-                string str = Console.ReadLine();
-                if (str.ToLower() == "exit")
-                    break;
+            Console.WriteLine("Enter a number to calculate or Type 'exit' for exit application");
+            string str = Console.ReadLine();
+            if (str.ToLower() == "exit")
+                return;
 
-                int input;
-                if (int.TryParse(str, out input))
+            int input;
+            if (int.TryParse(str, out input))
+            {
+                try
                 {
-                    try
-                    {
-                        calculateService.Calculate(input);
-                    }
-                    catch (EndpointNotFoundException)
-                    {
-                        Console.WriteLine("Run the server before running the client! Enter for exit");
-                        Console.ReadLine();
-                        break;
-                    }
-                    catch (CommunicationException) 
-                    {
-                        Console.WriteLine("An error occured during channel communication! Enter for exit");
-                        Console.ReadLine();
-                        break;
-                    }
+                    calculateService.Calculate(input);
                 }
-                else
+                catch (EndpointNotFoundException)
                 {
-                    Console.WriteLine("please enter a number!");
+                    Console.WriteLine("Run the server before running the client!");
+                }
+                catch (CommunicationException)
+                {
+                    Console.WriteLine("An error occured during channel communication!");
+                }
+                finally
+                {
+                    var communicationObject = calculateService as ICommunicationObject;
+                    if (communicationObject != null && communicationObject.State == CommunicationState.Opened)
+                        communicationObject.Close();
                 }
             }
-
-            var communicationObject = calculateService as ICommunicationObject;
-            if (communicationObject != null && communicationObject.State == CommunicationState.Opened)
-                communicationObject.Close();
-            
+            else
+            {
+                Console.WriteLine(string.Format("{0} is not a number!", str));
+            }
         }
     }
 }
